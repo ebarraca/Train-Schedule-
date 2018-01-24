08c6b87f4827734392f5e2aa4f880aaab8b949bd
Erica Barraca <ericabarraca@dhcp-10-105-138-67.wireless.northwestern.private>
2018-01-24 17:53:00 -0600

    final push

--- train.js
+++ train.js
@@ -24,12 +24,10 @@ var database = firebase.database();
  $("#submit-btn").on("click", function(event) {
      event.preventDefault();
 
-    var trainName = $("#name-input").val();
-    var destination = $("#destination-input").val();
-    var firstTrain = $("#first-train-input").val();
-    var frequency = $("#frequency-input").val();
-
-    console.log(trainName, destination, firstTrain, frequency);
+      var trainName = $("#name-input").val();
+      var destination = $("#destination-input").val();
+      var firstTrain = $("#first-train-input").val();
+      var frequency = $("#frequency-input").val();
 
       var newEntry = {
       trainName: trainName,
@@ -38,10 +36,10 @@ var database = firebase.database();
       frequency: frequency,
       };
 
-  // Uploads employee data to the database
+    // Uploads employee data to the database
       database.ref().push(newEntry);
 
-});
+ });
 
 // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
@@ -54,66 +52,45 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   var firstTrain = childSnapshot.val().firstTrain;
   var frequency = childSnapshot.val().frequency;
 
-  // Train Info
-  console.log(trainName);
-  console.log(destination);
-  console.log(firstTrain);
-  console.log(frequency);
+//moment() get current time
+  currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+  
+  firstTime = moment(firstTrain, "hh:mm").subtract(1, "years");
+  console.log(firstTime); 
 
-  //moment() get current time
-    currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-    
-    firstTime = moment(firstTrain, "hh:mm").subtract(1, "years");
-    console.log(firstTime); 
+  // Difference between the times
+  timeDifference = moment().diff(moment(firstTime), "minutes");
+  console.log(timeDifference);
 
-    // Difference between the times
-    timeDifference = moment().diff(moment(firstTime), "minutes");
-    console.log(timeDifference);
+  // Time apart (remainder)
+  tRemainder = timeDifference % frequency;
+  console.log(tRemainder);
 
-    // Time apart (remainder)
-    tRemainder = timeDifference % frequency;
-    console.log(tRemainder);
+  // Minute Until Train - need to change to moment js; need a readable time.
+  tMinutesTillTrain = frequency - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-    // Minute Until Train - need to change to moment js; need a readable time.
-    tMinutesTillTrain = frequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+  // Next Train
+  nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
-    // Next Train
-    nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
-    //.html
-    //timeApart, nextTrain, tMinutesTillTrain, 
-
-    // alert("Train schedule successfully added");
-
-      // Clears all of the text-boxes
-      $("#name-input").val("");
-      $("#destination-input").val("");
-      $("#first-train-input").val("");
-      $("#frequency").val("");
-
-
-  //   // Add each train's data into the table
-  // $("#train-table > tbody").append("<tr><th>" + trainName + "</th><th>" + destination + "</th><th>" +
-  // firstTrain + "</th><th>" + frequency + "</th><th>" );
+  // Clears all of the text-boxes
+  $("#name-input").val("");
+  $("#destination-input").val("");
+  $("#first-train-input").val("");
+  $("#frequency").val("");
 
 // Add each train's data into the table
  $("tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + moment(nextTrain).format("h:mm") + "</td><td>" + tMinutesTillTrain + "</td></tr>");
 
- //parseint - look at employee schedule 
 
 });
 });
 
 
 
-
-
-
-
-//refer to 02-recentuserwithpush.html for more notes
+//my notes: refer to 02-recentuserwithpush.html for more notes
 //do the firebase set up; var database = database.firebase AFTER the call; (be
 //sure to include firebase url to script)
 //set inital variables
